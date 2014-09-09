4b92343029023c7b8e075311968211a77a50e7c9
Derek Wilson <derek.wilson@bigfoot.com>
2014-09-09 18:48:39 +0100

    initial commit

--- javascript/pagescroll.js
+++ javascript/pagescroll.js
@@ -1,10 +1,10 @@
 function pageScroll() {
-	window.scrollBy(0,5); // horizontal and vertical scroll increments
-	scrolldelay = setTimeout('pageScroll()',100); // scrolls every 100 milliseconds
+    	window.scrollBy(0,5); // horizontal and vertical scroll increments
+    	scrolldelay = setTimeout('pageScroll()',100); // scrolls every 100 milliseconds
 }
 
 function redirect(x) {
-	//document.URL(x);
+    	//document.URL(x);
 	//window.location.assign(x);
 	location.replace(x);
 }
@@ -26,26 +26,27 @@ function validatenum2(fld) {
     if (ll > 0) {
 	chr= ff.value.substr(ll-1, 1);
 	if (chr == " ") {
-		// Chop off the final blank
-		ff.value= ff.value.substr(0,ll-1);
-		id = ff.tabIndex; // will be tabindexnnn
-		var elems= document.getElementsByTagName("input");
-		for(i=0; i< elems.length ; i++){
+	    // Chop off the final blank
+	    ff.value= ff.value.substr(0,ll-1);
+	    id = ff.tabIndex; // will be tabindexnnn
+	    var elems= document.getElementsByTagName("input");
+	    for(i=0; i< elems.length ; i++){
 		if (elems[i].tabIndex == (1 + id)) {
-			elems[i].focus();
-			elems[i].select();
-			return;
-			}
+		    elems[i].focus();
+		    elems[i].select();
+		    elems[i].focus();
+		    return;
+		    }
 		}
-		for(i=0; i< elems.length ; i++){
+	    for(i=0; i< elems.length ; i++){
 		if (elems[i].tabIndex == 1){
-			elems[i].focus();
-			elems[i].select();
-			return;
-			}
+		    elems[i].focus();
+		    return;
+		    }
 		}
-		}
-		return; // not a blank
+	    }
+	return; // not a blank
 	}
-	return; // length zero
-}
+    return; // length zero
+    }
+
