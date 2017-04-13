5ea90f3fed44b5570d297979e2fd2c0df444f0b4
Derek Wilson <derek.wilson@bigfoot.com>
2017-04-13 09:30:53 +0100

    Scroll parameters

--- javascript/pagescroll.js
+++ javascript/pagescroll.js
@@ -2,15 +2,23 @@ function leaderScroll() {
 	x = window.innerHeight; // Total window available
 	x -= document.getElementById("tablebody").offsetTop; // Subtract already written
 	x -= 50; // Leave 50px at bottom
-	document.getElementById("tablebody").style.height = x // Resize
+	document.getElementById("tablebody").style.height = x; // Resize
 
 	// First delay is two seconds
-	scrolldelay = setTimeout('tableScroll()', 5000); 
+	tm = document.getElementById("initial").innerHTML;
+	tm = parseFloat(tm).toFixed(3);
+	tm *= 1000;
+	scrolldelay = setTimeout('tableScroll()', tm); 
 }
 
 function tableScroll(){
-	document.getElementById("tablebody").scrollTop += 1;
-    	scrolldelay = setTimeout('tableScroll()',25); // scrolls every 100 milliseconds
+	pixel = document.getElementById("scrollpixel").innerHTML;
+	pixel = parseInt(pixel);
+	document.getElementById("tablebody").scrollTop += pixel;
+	delay = document.getElementById("scrolltime").innerHTML;
+	delay = parseFloat(delay);
+	delay *= 1000;
+   	scrolldelay = setTimeout('tableScroll()',delay); // scrolls every 100 milliseconds
 }
 
 function pageScroll() {
